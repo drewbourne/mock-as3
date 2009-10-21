a2cf75b00bd0b32eabc7a2006efde1ac09c8081b
firstbourne <firstbourne@bb1cc804-513f-0410-8659-4d20a89a40e2>
2009-10-21 22:05:45 +0000

    new swc.sh build script by offby1, requires only FLEX_HOME to be set.
    git-svn-id: https://mock-as3.googlecode.com/svn/trunk@29 bb1cc804-513f-0410-8659-4d20a89a40e2

--- core/build/swc.sh
+++ core/build/swc.sh
@@ -1,4 +1,14 @@
 # compile src to .swc
-compc \
- -include-sources ../src \
- -output ../bin/mock-as3.swc
+FLEX_HOME="${FLEX_HOME:?'FLEX_HOME must be set'}"
+PROJECT_DIR=$(unset CDPATH; cd `dirname $0`/..; pwd)
+COMPC="${FLEX_HOME}/bin/compc"
+"$COMPC" \
+ -source-path $PROJECT_DIR/src \
+ -include-sources $PROJECT_DIR/src \
+ -compiler.include-libraries \
+ $PROJECT_DIR/libs/Floxy.swc \
+ $PROJECT_DIR/libs/FlexUnit4.swc \
+ $PROJECT_DIR/libs/FlexUnit4UIRunner.swc \
+ $PROJECT_DIR/libs/asx.swc \
+ $PROJECT_DIR/libs/hamcrest.swc \
+ -output $PROJECT_DIR/bin/mock-as3.swc
